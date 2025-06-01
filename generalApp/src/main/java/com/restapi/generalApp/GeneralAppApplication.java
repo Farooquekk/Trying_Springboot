f640df7026d64ef2c7bc1a94bccc3c78f32a9416
Farooquekk <22sw040@students.muet.edu.pk>
2025-06-01 07:40:14 -0700

    trying springboot.

---
+++ generalApp/src/main/java/com/restapi/generalApp/GeneralAppApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.generalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralAppApplication.class, args);
+	}
+
+}
---
+++ generalApp/src/test/java/com/restapi/generalApp/GeneralAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.generalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
